02cc6f5e6aaa8f918927a96e06bae82f6ddc7094
ogait-ashtar <ogait.desenvolvedor.jr@gmail.com>
2022-12-01 20:36:10 -0300

    feat: create nest base structur to genero

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { UserController } from './user/user.controller';
 import { UserRepository } from './user/user.repository';
 import { ProfileModule } from './profile/profile.module';
 import { GamesModule } from './games/games.module';
+import { GenerosModule } from './generos/generos.module';
 
 @Module({
-  imports: [DatabaseModule, ProfileModule, GamesModule],
+  imports: [DatabaseModule, ProfileModule, GamesModule, GenerosModule],
   controllers: [UserController],
   providers: [UserService, UserRepository],
 })
