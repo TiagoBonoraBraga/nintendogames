b93ac02d7344cf065e9b25b98ddffedd80644cd4
ogait-ashtar <ogait.desenvolvedor.jr@gmail.com>
2022-12-01 19:44:22 -0300

    feat: generate games entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { UserService } from './user/services/user.service';
 import { UserController } from './user/user.controller';
 import { UserRepository } from './user/user.repository';
 import { ProfileModule } from './profile/profile.module';
+import { GamesModule } from './games/games.module';
 
 @Module({
-  imports: [DatabaseModule, ProfileModule],
+  imports: [DatabaseModule, ProfileModule, GamesModule],
   controllers: [UserController],
   providers: [UserService, UserRepository],
 })
