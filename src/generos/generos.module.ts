0d3eaafbf899422b89ba6641d9037328cea10bc4
ogait-ashtar <ogait.desenvolvedor.jr@gmail.com>
2022-12-06 19:51:31 -0300

    feat: genero.module

--- src/generos/generos.module.ts
+++ src/generos/generos.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { GenerosService } from './generos.service';
 import { GenerosController } from './generos.controller';
+import { GeneroRepository } from './genero.repository';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
   controllers: [GenerosController],
-  providers: [GenerosService],
+  providers: [GenerosService, GeneroRepository, PrismaService],
 })
 export class GenerosModule {}
