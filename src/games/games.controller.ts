e14346bb9d0ca1c8aae2bfd1606a06e56712b974
ogait-ashtar <ogait.desenvolvedor.jr@gmail.com>
2022-12-06 21:57:06 -0300

    feat: games.controller

--- src/games/games.controller.ts
+++ src/games/games.controller.ts
@@ -6,37 +6,72 @@ import {
   Patch,
   Param,
   Delete,
+  Res,
 } from '@nestjs/common';
 import { GamesService } from './games.service';
 import { CreateGameDto } from './dto/create-game.dto';
 import { UpdateGameDto } from './dto/update-game.dto';
+import { Response } from 'express';
+import { handleException } from 'src/utils/exceptions/exceptionsHelper';
+import { Game } from './entities/game.entity';
 
 @Controller('games')
 export class GamesController {
   constructor(private readonly gamesService: GamesService) {}
 
   @Post()
-  create(@Body() createGameDto: CreateGameDto) {
-    return this.gamesService.create(createGameDto);
+  async createGame(
+    @Body() { name, descricao, year, autor, produtora, score }: CreateGameDto,
+    @Res() response: Response,
+  ) {
+    try {
+      const result = await this.gamesService.createGame({
+        name,
+        descricao,
+        year,
+        autor,
+        produtora,
+        score,
+      });
+      response.status(201).send(result);
+    } catch (error) {
+      handleException(error);
+    }
   }
 
   @Get()
-  findAll() {
-    return this.gamesService.findAll();
+  async getAllGames(): Promise<Game[]> {
+    return this.gamesService.getAllGames();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.gamesService.findOne(+id);
+  async getGameById(@Param('id') gameId: string): Promise<Game> {
+    try {
+      return await this.gamesService.getGameById(gameId);
+    } catch (error) {
+      handleException(error);
+    }
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateGameDto: UpdateGameDto) {
-    return this.gamesService.update(+id, updateGameDto);
+  async updateGame(
+    @Param('id') id: string,
+    @Body() gameData: UpdateGameDto,
+  ): Promise<UpdateGameDto> {
+    try {
+      return await this.gamesService.updateGame(id, gameData);
+    } catch (error) {
+      handleException(error);
+    }
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.gamesService.remove(+id);
+  async DeleteGameById(@Param('id') gameId: string): Promise<string> {
+    const gameIsDeleted = await this.gamesService.deleteGameById(gameId);
+    if (gameIsDeleted) {
+      return 'Game deletado com sucesso';
+    } else {
+      return 'Game não encontrado';
+    }
   }
 }
